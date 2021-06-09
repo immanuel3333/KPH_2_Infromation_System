b122496ff5fe3c02512119f51d018bde37987b29
Purify12 <marchelsitumorang2000@gmail.com>
2021-06-09 11:10:02 +0700

    menambahkan auth

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Covid Checking - @yield('title')</title>
+
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" />
+      <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('assets/css/backend.css?v=1.0.0') }}">
+      <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/fonts/remixicon.css') }}">
+      <link rel="stylesheet" href="{{ asset('assets/vendor/@icon/dripicons/dripicons.css') }}">
+
+      <link rel='stylesheet' href='{{ asset('assets/vendor/fullcalendar/core/main.css') }}' />
+      <link rel='stylesheet' href='{{ asset('assets/vendor/fullcalendar/daygrid/main.css') }}' />
+      <link rel='stylesheet' href='{{ asset('assets/vendor/fullcalendar/timegrid/main.css') }}' />
+      <link rel='stylesheet' href='{{ asset('assets/vendor/fullcalendar/list/main.css') }}' />
+      <link rel="stylesheet" href="{{ asset('assets/vendor/mapbox/mapbox-gl.css') }}">
+
+      <link rel="stylesheet" href="{{ asset('css/cc.css') }}">
+
+      </head>
+  <body class="bg">
+      <div class="wrapper">
+      <section class="login-content">
+         <div class="container h-100">
+            <div class="row align-items-center justify-content-center h-100">
+               <div class="col-md-5">
+                  @yield('content')
+               </div>
+            </div>
+         </div>
+      </section>
+      </div>
+
+    <!-- Backend Bundle JavaScript -->
+    <script src="{{ asset('assets/js/backend-bundle.min.js') }}"></script>
+
+    <!-- Flextree Javascript-->
+    <script src="{{ asset('assets/js/flex-tree.min.js') }}"></script>
+    <script src="{{ asset('assets/js/tree.js') }}"></script>
+
+    <!-- Table Treeview JavaScript -->
+    <script src="{{ asset('assets/js/table-treeview.js') }}"></script>
+
+    <!-- Masonary Gallery Javascript -->
+    <script src="{{ asset('assets/js/masonry.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+
+    <!-- Mapbox Javascript -->
+    <script src="{{ asset('assets/js/mapbox-gl.js') }}"></script>
+    <script src="{{ asset('assets/js/mapbox.js') }}"></script>
+
+    <!-- Fullcalender Javascript -->
+    <script src='{{ asset('assets/vendor/fullcalendar/core/main.js') }}'></script>
+    <script src='{{ asset('assets/vendor/fullcalendar/daygrid/main.js') }}'></script>
+    <script src='{{ asset('assets/vendor/fullcalendar/timegrid/main.js') }}'></script>
+    <script src='{{ asset('assets/vendor/fullcalendar/list/main.js') }}'></script>
+
+    <!-- SweetAlert JavaScript -->
+    <script src="{{ asset('assets/js/sweetalert.js') }}"></script>
+
+    <!-- Vectoe Map JavaScript -->
+    <script src="{{ asset('assets/js/vector-map-custom.js') }}"></script>
+
+    <!-- Chart Custom JavaScript -->
+    <script src="{{ asset('assets/js/customizer.js') }}"></script>
+
+    <!-- Chart Custom JavaScript -->
+    <script src="{{ asset('assets/js/chart-custom.js') }}"></script>
+    <script src="{{ asset('assets/js/charts/01.js') }}"></script>
+    <script src="{{ asset('assets/js/charts/02.js') }}"></script>
+
+    <!-- slider JavaScript -->
+    <script src="{{ asset('assets/js/slider.js') }}"></script>
+
+    <!-- Emoji picker -->
+    <script src="{{ asset('assets/vendor/emoji-picker-element/index.js') }}" type="module"></script>
+
+    <!-- app JavaScript -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>  </body>
+</html>
